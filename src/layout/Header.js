4745b9c0231fadf9b5539b637d80afb35b1048ff
Osama Abu Safieh <131916263+Osamaas93@users.noreply.github.com>
2024-02-20 03:39:54 +0200

    responsive home page

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,14 +1,35 @@
 import { Button, ButtonGroup, Stack } from "@mui/material";
 import styled from "styled-components";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
+import MenuList from "@mui/material/MenuList";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Box } from "@mui/system";
+import { useState, useEffect } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const CustomButton = styled(Button)`
   && {
+    position: relative;
     color: var(--brandPrimary);
     font-weight: 600;
+    /* Define styles for gutterBottom transient prop */
+    margin-bottom: ${(props) => (props.$gutterBottom ? "8px" : "0")};
   }
 `;
 
-const ResumeButton = styled(Button)`
+const CustomBox = styled(Box)`
+  && {
+    position: absolute;
+    top: 65px;
+    background: var(--bgDefault);
+    border-radius: 8px;
+  }
+`;
+
+const ResumeButton = styled(MenuItem)`
   && {
     color: var(--brandSecondary);
     font-weight: 600;
@@ -17,23 +38,69 @@ const ResumeButton = styled(Button)`
 const CustomImg = styled.img`
   width: 33px;
 `;
+
 const Header = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 780);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Stack direction="row" spacing={2} justifyContent="center" my={3}>
       <CustomImg src={`${process.env.PUBLIC_URL}/assets/html.png`} alt="awd" />
 
-      <ButtonGroup variant="text" aria-label="button group">
-        <CustomButton>Frontend Development</CustomButton>
-        <CustomButton>Music Production</CustomButton>
-        <CustomButton>Contact Me</CustomButton>
-      </ButtonGroup>
-      <ResumeButton
-        component="a"
-        href="https://drive.google.com/file/d/1_Zqm_A46HNvBCaIsFt2CXGMKRJrKhNro/view?usp=sharing"
-      >
-        Download Resume
-      </ResumeButton>
+      {isSmallScreen ? (
+        <>
+          <CustomButton onClick={() => setOpen(!open)}>
+            <MenuIcon />
+          </CustomButton>
+          {open && (
+            <CustomBox>
+              <MenuList dense>
+                <MenuItem>
+                  <ListItemText>Frontend Development</ListItemText>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemText>Music Production</ListItemText>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemText>Contact Me</ListItemText>
+                </MenuItem>
+                <ResumeButton
+                  component="a"
+                  href="https://drive.google.com/file/d/1_Zqm_A46HNvBCaIsFt2CXGMKRJrKhNro/view?usp=sharing"
+                >
+                  Download Resume
+                </ResumeButton>
+              </MenuList>
+            </CustomBox>
+          )}
+        </>
+      ) : (
+        <ButtonGroup variant="text" aria-label="button group">
+          <CustomButton>Frontend Development</CustomButton>
+          <CustomButton>Music Production</CustomButton>
+          <CustomButton>Contact Me</CustomButton>
+          <ResumeButton
+            component="a"
+            href="https://drive.google.com/file/d/1_Zqm_A46HNvBCaIsFt2CXGMKRJrKhNro/view?usp=sharing"
+          >
+            Download Resume
+          </ResumeButton>
+        </ButtonGroup>
+      )}
     </Stack>
   );
 };
+
 export default Header;
--- src/pages/InformationPage.js
+++ src/pages/InformationPage.js
@@ -2,60 +2,82 @@ import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import styled from "styled-components";
 import { myInformation } from "../utls/myInfotmation";
-const Img = styled("img")({
-  margin: "auto",
-  display: "block",
-  maxWidth: "400px",
-  maxHeight: "100%",
-});
+
+const Img = styled("img")`
+  && {
+    margin: auto;
+    display: block;
+    max-width: 400px;
+    max-height: 100%;
+    @media (max-width: 700px) {
+      max-width: 300px;
+    }
+  }
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 const ImgBackground = styled.div`
   display: flex;
+  flex-shrink: 0;
+  justify-content: center;
   width: 430px;
   height: 430px;
-  flex-shrink: 0;
   border-radius: 528px;
   background: #444;
   box-shadow: 0px 0px 50px 0px rgba(0, 0, 0, 0.8);
+  @media (max-width: 700px) {
+    width: 325px;
+    height: 325px;
+  }
 `;
 
 const Title = styled.h2`
   font-size: 2rem;
   color: var(--brandSecondary);
   font-weight: 700;
+  @media (max-width: 700px) {
+    font-size: 1.5rem;
+  }
 `;
 
 const Description = styled.h4`
   font-size: 1.2rem;
   color: var(--bodyText);
   font-weight: 500;
+  @media (max-width: 700px) {
+    font-size: 1rem;
+  }
 `;
 
 const InformationPage = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid item>
-        <ImgBackground>
-          <Img
-            alt="complex"
-            src={`${process.env.PUBLIC_URL}/assets/frontend-img.png`}
-          />
-        </ImgBackground>
+    <Grid container spacing={3}>
+      <Grid item xs={12} lg={5}>
+        <ImgContainer>
+          <ImgBackground>
+            <Img
+              alt="complex"
+              src={`${process.env.PUBLIC_URL}/assets/frontend-img.png`}
+            />
+          </ImgBackground>
+        </ImgContainer>
       </Grid>
       <Grid
         item
         xs={12}
-        sm
+        lg={7}
         container
+        direction="column"
         justifyContent="center"
-        alignItems="center"
+        alignItems="center" // Center the content vertically
       >
-        <Grid item xs container direction="column" spacing={2}>
-          <Grid item xs>
-            <Title>{myInformation.frontend.title}</Title>
-            <Description variant="body2" gutterBottom>
-              {myInformation.frontend.description}
-            </Description>
-          </Grid>
+        <Grid item>
+          <Title>{myInformation.frontend.title}</Title>
+          <Description>{myInformation.frontend.description}</Description>
         </Grid>
       </Grid>
     </Grid>
